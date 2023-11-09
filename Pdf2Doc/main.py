a630c503b0c605c0bce1e034cee229b7773b709a
JobaBoba <legoga@gmail.com>
2023-11-09 12:39:37 +0100

    Password generator

--- Pdf2Doc/main.py
+++ Pdf2Doc/main.py
@@ -1,33 +1,24 @@
-# Импорт библиотек
 import tkinter as tk
 from tkinter import filedialog
-from pdf2docx import parse
-from typing import Tuple
+import fitz  # PyMuPDF
 
-def convert_pdf2docx(input_file: str, output_file: str, pages: Tuple = None):
-    """Конвертирует pdf в docx"""
-    if pages:
-        pages = [int(i) for i in list(pages) if i.isnumeric()]
-    result = parse(pdf_file=input_file, docx_with_path=output_file, pages=pages)
-    summary = {
-        "File": input_file,
-        "Pages": str(pages),
-        "Output File": output_file
-    }
-    # Вывод результата
-    print("## Summary ########################################################")
-    print("\\n".join(" {}: {}".format(i, j) for i, j in summary.items()))
-    print("###################################################################")
-    return result
+def convert_pdf2docx(input_file: str, output_file: str):
+    """ Конвертирует PDF в DOCX. """
+    doc = fitz.open(input_file)
+    doc.save(output_file, garbage=4, deflate=True, clean=True)
 
 def browse_files():
-    """Открывает диалоговое окно для выбора файла"""
+    """ Открывает диалоговое окно для выбора файла. """
     root = tk.Tk()
-    root.withdraw()  # скрыть основное окно
-    file_path = filedialog.askopenfilename()
+    root.withdraw()  # Скрытие основного окна Tkinter
+    file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
     return file_path
 
 if __name__ == "__main__":
     input_file = browse_files()
-    output_file = input_file.replace('.pdf', '.docx')
-    convert_pdf2docx(input_file, output_file)
+    if input_file:
+        output_file = input_file.replace('.pdf', '.docx')
+        convert_pdf2docx(input_file, output_file)
+        print(f"Файл {output_file} был успешно создан.")
+    else:
+        print("Файл не выбран.")
